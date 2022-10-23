e50cc3c1e4a7b26611032c10d65e7f860b56185f
admin <gust0021542@gust.edu.kw>
2022-10-23 19:38:56 +0300

    Saved recipe: TWITTERGEOANALYSIS.compute_Twi_Data_Split_Text_Clean

---
+++ recipes/compute_Twi_Data_Split_Text_Clean.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+Twi_Data_Cleaned_Cols_Parsed = dataiku.Dataset("Twi_Data_Cleaned_Cols_Parsed")
+Twi_Data_Cleaned_Cols_Parsed_df = Twi_Data_Cleaned_Cols_Parsed.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+Twi_Data_Split_Text_Clean_df = Twi_Data_Cleaned_Cols_Parsed_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+Twi_Data_Split_Text_Clean = dataiku.Dataset("Twi_Data_Split_Text_Clean")
+Twi_Data_Split_Text_Clean.write_with_schema(Twi_Data_Split_Text_Clean_df)
