4c80f46400159d566d279d6f8767d810f6f16732
admin <gust0021542@gust.edu.kw>
2022-10-24 14:26:19 +0300

    Recipe TWITTERGEOANALYSIS.compute_Twi_Data_Place_Name_Extract saved through notebook notebook editor for compute_Twi_Data_Place_Name_Extract

--- recipes/compute_Twi_Data_Place_Name_Extract.py
+++ recipes/compute_Twi_Data_Place_Name_Extract.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -7,11 +8,21 @@ from dataiku import pandasutils as pdu
 Twi_Data_prepared = dataiku.Dataset("Twi_Data_prepared")
 Twi_Data_prepared_df = Twi_Data_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# make new column that says place_name
+place_names = []
+# TODO: loop through text and extract 1 or 2 words after "I'm at"
+for text in Twi_Data_prepared_df['tweet_text_clean']:
+    split_text = text.lower().split()
+    if split_text[0] == "i'm" and split_text[1] == 'at':
+        place_names.append(''.join(split_text[2:4]))
+    else:
+        place_names.append(text)
+    # TODO: split text
+    # TODO: if first 2 elements are I'm and at then grab the next 2 words
+    # TODO: work from there
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 Twi_Data_Place_Name_Extract_df = Twi_Data_prepared_df # For this sample code, simply copy input to output
 
 
