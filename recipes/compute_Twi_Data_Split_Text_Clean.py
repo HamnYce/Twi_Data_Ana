a8a4becc01a87dc4424d8afd7d9ffa97f6c290b5
admin <gust0021542@gust.edu.kw>
2022-10-25 21:06:29 +0300

    Recipe TWITTERGEOANALYSIS.compute_Twi_Data_Split_Text_Clean saved through notebook notebook editor for compute_Twi_Data_Split_Text_Clean_2

--- recipes/compute_Twi_Data_Split_Text_Clean.py
+++ recipes/compute_Twi_Data_Split_Text_Clean.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -7,11 +8,62 @@ from dataiku import pandasutils as pdu
 Twi_Data_cols_parsed = dataiku.Dataset("Twi_Data_cols_parsed")
 Twi_Data_cols_parsed_df = Twi_Data_cols_parsed.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+Twi_Data_cols_parsed_df['text'] = tweet_text_clean
+Twi_Data_cols_parsed_df['text_first_link'] = first_link_clean
+Twi_Data_cols_parsed_df['text_second_link'] = second_link_clean
+Twi_Data_cols_parsed_df['text_third_link'] = third_link_clean
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+tweet_text_clean = []
+for text in tweet_text:
+    clean_text = text
+    if text[0] == "'" or text[0] == '"':
+        clean_text = text[1:]
+    if text[-1] == "'" or text[-1] == '"':
+        clean_text = text[:-1]
+    tweet_text_clean.append(clean_text)
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+first_link_clean = []
+for text in first_link:
+    clean_text = text
+    if type(clean_text) == str:
+        if text[0] == "'" or text[0] == '"':
+            clean_text = text[1:]
+        if text[-1] == "'" or text[-1] == '"':
+            clean_text = text[:-1]
+    first_link_clean.append(clean_text)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+second_link_clean = []
+for text in second_link:
+    clean_text = text
+    if type(clean_text) == str:
+        if text[0] == "'" or text[0] == '"':
+            clean_text = text[1:]
+        if text[-1] == "'" or text[-1] == '"':
+            clean_text = text[:-1]
+    second_link_clean.append(clean_text)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+third_link_clean = []
+for text in second_link:
+    clean_text = text
+    if type(clean_text) == str:
+        if text[0] == "'" or text[0] == '"':
+            clean_text = text[1:]
+        if text[-1] == "'" or text[-1] == '"':
+            clean_text = text[:-1]
+    third_link_clean.append(clean_text)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+Twi_Data_cols_parsed_df['text'] = tweet_text_clean
+Twi_Data_cols_parsed_df['text_first_link'] = first_link_clean
+Twi_Data_cols_parsed_df['text_second_link'] = second_link_clean
+Twi_Data_cols_parsed_df['text_third_link'] = third_link_clean
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 Twi_Data_Split_Text_Clean_df = Twi_Data_cols_parsed_df # For this sample code, simply copy input to output
 
 
