4823f5740a62776f9000037ab734a02f4f5da898
admin <gust0021542@gust.edu.kw>
2022-10-25 23:33:36 +0300

    Recipe TWITTERGEOANALYSIS.compute_Twi_Data_Place_Name_Clean saved through notebook notebook editor for compute_Twi_Data_Place_Name_Clean

--- recipes/compute_Twi_Data_Place_Name_Clean.py
+++ recipes/compute_Twi_Data_Place_Name_Clean.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -7,11 +8,16 @@ from dataiku import pandasutils as pdu
 Twi_Data_Place_Name_From_Text = dataiku.Dataset("Twi_Data_Place_Name_From_Text")
 Twi_Data_Place_Name_From_Text_df = Twi_Data_Place_Name_From_Text.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+place_names = Twi_Data_Place_Name_From_Text_df['place_name']
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+stripped_place_names = []
+for place in place_names:
+    stripped_place_names.append(place..strip("'").strip('"'))
+stripped_place_names
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 Twi_Data_Place_Name_Clean_df = Twi_Data_Place_Name_From_Text_df # For this sample code, simply copy input to output
 
 
